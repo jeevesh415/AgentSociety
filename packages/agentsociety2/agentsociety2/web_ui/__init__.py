23897d484e0b5bd3ec53aad7a125da5d9ca3e893
Jun Zhang <zhangjun990222@qq.com>
2026-02-25 23:06:04 +0800

    remove chainlit webui

--- packages/agentsociety2/agentsociety2/web_ui/__init__.py
+++
@@ -1,8 +0,0 @@
-from .io_adapter import ChainlitIOAdapter, smart_print, get_current_io_adapter, chainlit_compatible
-
-__all__ = [
-    "ChainlitIOAdapter",
-    "smart_print",
-    "get_current_io_adapter",
-    "chainlit_compatible"
-]
